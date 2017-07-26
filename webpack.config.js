a5d0e6b8a6dc7d11fb14ee75ab39e7e710e5a02f
iamsail <865605793@qq.com>
2017-07-26 23:53:41 +0800

    调整lujign

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devtool: 'eval-source-map',//配置生成Source Maps,选择合适的选项
     entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
-        path: __dirname + "/public",//打包后的文件存放的地方
+        path: __dirname + "/app/public",//打包后的文件存放的地方
         filename: "bundle.js"//打包后输出文件的文件名
     },
     module: {//在配置文件里添加JSON loader
@@ -36,7 +36,7 @@ module.exports = {
                     return [precss, autoprefixer];
                 },
                 devServer: {
-                    contentBase: "./public", //本地服务器所加载的页面所在的目录
+                    contentBase: "./app/public", //本地服务器所加载的页面所在的目录
                     port: "8080",
                     colors: true, //终端中输出结果为彩色
                     historyApiFallback: true, //不跳转
